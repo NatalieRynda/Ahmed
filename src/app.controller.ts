949954afe6c0a3cd64b1211d6481386242b3e25c
NatalieRynda <106041720+NatalieRynda@users.noreply.github.com>
2022-06-27 22:33:01 -0400

    final

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,35 +5,35 @@ import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
- @Get("users")
- findAllUsers() {
-  return this.appService.findAll()
- } 
+  @Get("users")
+  findAllUsers() {
+    return this.appService.findAll()
+  }
 
- @Post("users")
- createUser(@Body() userDto: CreateUserDto) {
-  return this.appService.create(userDto)
- } 
+  @Post("users")
+  createUser(@Body() userDto: CreateUserDto) {
+    return this.appService.create(userDto)
+  }
 
- @Delete("users/:id")
- deleteUser(@Param("id") id: number) {
-  return this.appService.remove(id)
- } 
+  @Delete("users/:id")
+  deleteUser(@Param("id") id: number) {
+    return this.appService.remove(id)
+  }
 
- @Get("users/:id")
- findUser(@Param("id") id: number) { 
-  return this.appService.findOne(id)
- } 
+  @Get('users/:id')
+  findUser(@Param('id') id: number) {
+    return this.appService.findOne(id)
+  }
 
- @Get("users/:id/firstName")
- find1User(@Param("id") id: number) {
-  return this.appService.find1Cust(id)
- } 
+  @Get("users/:id/firstName")
+  find1User(@Param("id") id: number) {
+    return this.appService.find1Cust(id)
+  }
 
- @Patch("users/:id")
- updateUser(@Param("id") id: number, @Body() userDto: UpdateUserDto) {
-  return this.appService.update(userDto,id)
- } 
+  @Patch("users/:id")
+  updateUser(@Param("id") id: number, @Body() userDto: UpdateUserDto) {
+    return this.appService.update(userDto, id)
+  }
 }
